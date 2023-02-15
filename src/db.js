0c5aadc76978e1c55733d9655c932cd6e8822c80
lucasdiaz7 <lucasdiaz687@gmail.com>
2023-02-15 12:33:28 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const pg= require('pg')
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE 
 } = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false,
+  timestamps: false,
+  dialectModule: pg, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
-
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
