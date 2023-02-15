ff811443881070070abaae8e9a9004a55cccbdc9
lucasdiaz7 <lucasdiaz687@gmail.com>
2023-02-15 13:31:36 -0300

    aaa

--- src/db.js
+++ src/db.js
@@ -5,9 +5,9 @@ const path = require('path');
 const pg= require('pg')
 
 const {
-DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE 
+DB_USER, DB_PASSWORD, DB_HOST, PORT, DB_DATABASE 
 } = process.env;
-const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false,
   timestamps: false,
