1f3379729a88384b76b08e6293e0b84f9f1f7acb
lucasdiaz7 <lucasdiaz687@gmail.com>
2023-02-15 13:34:49 -0300

    q

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {PORT} = process.env
+const {PORT}= process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT || 3001, () => {
+  server.listen(PORT||7774, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
